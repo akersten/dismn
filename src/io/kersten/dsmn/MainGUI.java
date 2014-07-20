27f9c1da021006326769ed81aab05b9e5cbb2505
Alex Kersten <alex@dividebyxero.com>
2014-07-19 19:35:53 -0500

    broadcast addresses aren't right

--- src/io/kersten/dsmn/MainGUI.java
+++ src/io/kersten/dsmn/MainGUI.java
@@ -3,20 +3,33 @@ package io.kersten.dsmn;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.IOException;
 
 public class MainGUI extends JFrame {
     // IntelliJ makes these and hides them somewhere
     private JTextArea textArea1;
     private JTextField textField1;
     private JPanel rootPanel;
+    private JComboBox comboBox1;
+
+    private Transmitter host;
 
     public void addMessage(String message) {
         textArea1.append(message + "\n");
         textArea1.setCaretPosition(textArea1.getText().length());
     }
 
-    public MainGUI() {
+    public void addBroadcastGroup(String group) {
+        comboBox1.addItem(group);
+        comboBox1.setSelectedIndex(0);
+    }
+
+    public MainGUI(final Transmitter host) {
         super("dismn");
+
+        this.host = host;
+
+
         this.getContentPane().add(rootPanel);
 
 
@@ -41,7 +54,13 @@ public class MainGUI extends JFrame {
             @Override
             public void keyReleased(KeyEvent e) {
                 if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    try {
+                        host.transmit((String)comboBox1.getSelectedItem(), textField1.getText());
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    }
                     textField1.setText("");
+
                 }
             }
         });
--- src/io/kersten/dsmn/Transmitter.java
+++ src/io/kersten/dsmn/Transmitter.java
@@ -1,20 +1,90 @@
 package io.kersten.dsmn;
 
+import java.io.IOException;
+import java.net.*;
+import java.util.Enumeration;
+
 /**
  * dismn
  */
 public class Transmitter implements Runnable {
 
     MainGUI gui;
+    MulticastSocket socket;
 
     public Transmitter() {
-        MainGUI gui = new MainGUI();
+        MainGUI gui = new MainGUI(this);
 
 
+        // populate interfaces in the gui
+        System.setProperty("java.net.preferIPv4Stack", "true");
+        try
+        {
+            Enumeration<NetworkInterface> niEnum = NetworkInterface.getNetworkInterfaces();
+            while (niEnum.hasMoreElements())
+            {
+                NetworkInterface ni = niEnum.nextElement();
+                if (ni.isLoopback())
+                    continue;
+
+                System.out.println("new interface:" + ni.getDisplayName() + "\n");
+                for (InterfaceAddress interfaceAddress : ni.getInterfaceAddresses())
+                {
+
+
+                    if (interfaceAddress.getBroadcast() != null)
+                        gui.addBroadcastGroup(interfaceAddress.getBroadcast().toString());
+
+                    System.out.println("address" + interfaceAddress.getAddress().getHostAddress() + " name:"
+                            + interfaceAddress.getAddress().getHostName() + " prefix:"
+                            + interfaceAddress.getNetworkPrefixLength() + " broadcast:"
+                            + interfaceAddress.getBroadcast());
+
+                }
+            }
+        }
+        catch (SocketException e)
+        {
+            e.printStackTrace();
+        }
+//
+//        Enumeration<NetworkInterface> faces = null;
+//
+//        try {
+//            faces = NetworkInterface.getNetworkInterfaces();
+//            while (faces.hasMoreElements()) {
+//                NetworkInterface f = faces.nextElement();
+//
+//                    continue;
+//                for (InterfaceAddress interfaceAddress : f.getInterfaceAddresses()) {
+//                    InetAddress broadcast = interfaceAddress.getBroadcast();
+//                    if (broadcast != null) {
+//
+//                    }
+//                }
+//
+//            }
+//
+//        } catch (SocketException e) {
+//            e.printStackTrace();
+//        }
+//
+
+
+        try {
+            socket = new MulticastSocket(12345);
+        } catch (Exception e) {
+            // whatever
+            System.exit(1);
+        }
     }
 
-    public void transmit() {
+    public void transmit(String group, String msg) throws IOException {
+        InetAddress addr = InetAddress.getByName(group);
+        byte[] buf = msg.getBytes();
 
+        DatagramPacket packet = new DatagramPacket(buf, buf.length, addr, 12345);
+        socket.send(packet);
     }
 
     @Override
